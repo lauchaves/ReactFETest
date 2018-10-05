e32bd0311724b8bbbc2c9b0f189f3efd59849339
Lau Chaves <laurens004@gmail.com>
2018-10-05 16:48:05 -0600

    FIX: minor

--- src/containers/signup.js
+++ src/containers/signup.js
@@ -34,7 +34,7 @@ class SignUp extends Component {
     render() {
       const { redirect } = this.state;
       if (redirect){
-        return <Redirect to='/reports'/>;
+        return <Redirect to='/login'/>;
       }
       return <SignUpForm isSignUp={this.state.isSignUp} model={this.state.model}  handleSubmit={this.handleSubmit} />;
     };
