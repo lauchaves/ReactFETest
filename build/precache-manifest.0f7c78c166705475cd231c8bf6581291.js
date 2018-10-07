1f28a2e4a38be0e5d266fd6390a7c9325e0598c6
Lau Chaves <laurens004@gmail.com>
2018-10-07 14:07:33 -0600

    FEATURE: Deploy

---
+++ build/precache-manifest.0f7c78c166705475cd231c8bf6581291.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b20990e2fe26f57c29f",
+    "url": "/static/js/main.4b20990e.chunk.js"
+  },
+  {
+    "revision": "b9fa2136a380f83fb4b7",
+    "url": "/static/js/1.b9fa2136.chunk.js"
+  },
+  {
+    "revision": "4b20990e2fe26f57c29f",
+    "url": "/static/css/main.a0185e8d.chunk.css"
+  },
+  {
+    "revision": "ad89dc5c04408ea61d2a735ed4fbbbcd",
+    "url": "/index.html"
+  }
+];
